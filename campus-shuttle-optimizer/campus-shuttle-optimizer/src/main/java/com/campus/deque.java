52989e201bd0c5c6ae0bb4c1e058bb35067ad584
mel05-cloud <moresense05@gmail.com>
2026-08-09 17:42:47 +0000

    Add files via upload
    feat(structures): add linear data structures

---
+++ campus-shuttle-optimizer/campus-shuttle-optimizer/src/main/java/com/campus/deque.java
@@ -0,0 +1,79 @@
+package com.campus.optimizer.structures;
+
+import java.util.NoSuchElementException;
+
+public class Deque<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException("Deque is empty");
+        T data = head.data;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        size--;
+        return data;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) throw new NoSuchElementException("Deque is empty");
+        T data = tail.data;
+        tail = tail.prev;
+        if (tail != null) {
+            tail.next = null;
+        } else {
+            head = null;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+}public class Main {
+    
+}
