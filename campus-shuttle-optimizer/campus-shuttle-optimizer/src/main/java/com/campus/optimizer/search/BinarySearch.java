87a10f962f86448a43d924617a3f2ed80fa7383c
Etornam-KQG <ekgquashigah@st.ug.edu.gh>
2026-08-18 23:30:53 +0000

    Add binary search.java file

---
+++ campus-shuttle-optimizer/campus-shuttle-optimizer/src/main/java/com/campus/optimizer/search/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.campus.optimizer.search;
+
+public class BinarySearch {
+
+    // Returns the index of target, or -1 if target is not found.
+    // The array must be sorted in ascending order.
+    public static int search(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (array[middle] == target) {
+                return middle;
+            }
+
+            if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
