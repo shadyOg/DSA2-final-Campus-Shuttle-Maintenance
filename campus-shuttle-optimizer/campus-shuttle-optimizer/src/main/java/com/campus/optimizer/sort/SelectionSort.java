df79456322d7fce438c0aa33a467be68261249a9
Etornam-KQG <ekgquashigah@st.ug.edu.gh>
2026-08-18 23:37:35 +0000

    Add selection sort.java file

---
+++ campus-shuttle-optimizer/campus-shuttle-optimizer/src/main/java/com/campus/optimizer/sort/SelectionSort.java
@@ -0,0 +1,20 @@
+package com.campus.optimizer.sort;
+
+public class SelectionSort {
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
