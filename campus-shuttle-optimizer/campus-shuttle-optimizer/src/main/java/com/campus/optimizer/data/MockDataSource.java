91fa9c4f96c510b46f29e4ce9b75676789be5fcb
Fredrick1shim <fktsievor@st.ug.edu.gh>
2026-08-22 12:17:58 +0000

    Refactor MockDataSource to use DAO classes

--- campus-shuttle-optimizer/campus-shuttle-optimizer/src/main/java/com/campus/optimizer/data/MockDataSource.java
+++ campus-shuttle-optimizer/campus-shuttle-optimizer/src/main/java/com/campus/optimizer/data/MockDataSource.java
@@ -1,73 +1,44 @@
 package com.campus.optimizer.data;
 
-import com.campus.optimizer.model.Resource;
+import com.campus.optimizer.db.LocationDAO;
+import com.campus.optimizer.db.RoadDAO;
+import com.campus.optimizer.db.ServiceRequestDAO;
+import com.campus.optimizer.db.ResourceDAO;
 
-import java.util.ArrayList;
 import java.util.List;
+import com.campus.optimizer.model.Resource;
 
 public class MockDataSource implements DataSource {
 
-    private final List<Location> locations = new ArrayList<>();
-    private final List<Road> roads = new ArrayList<>();
-    private final List<ServiceRequest> serviceRequests = new ArrayList<>();
-    private final List<Resource> resources = new ArrayList<>();
+    private final LocationDAO locationDAO;
+    private final RoadDAO roadDAO;
+    private final ServiceRequestDAO serviceRequestDAO;
+    private final ResourceDAO resourceDAO;
 
     public MockDataSource() {
-        //locations
-        locations.add(new Location(1, "Balme Library"));
-        locations.add(new Location(2, "UGCS"));
-        locations.add(new Location(3, "Commonwealth Hall"));
-        locations.add(new Location(4, "Legon Hall"));
-        locations.add(new Location(5, "GCB"));
-        locations.add(new Location(6, "JQB"));
-        locations.add(new Location(7, "N Block"));
-        locations.add(new Location(8, "New N block"));
-
-        //roads
-        roads.add(new Road(1, 2, 0.8));
-        roads.add(new Road(2, 3, 0.6));
-        roads.add(new Road(3, 4, 0.5));
-        roads.add(new Road(4, 5, 0.7));
-        roads.add(new Road(5, 6, 0.4));
-        roads.add(new Road(6, 7, 0.9));
-        roads.add(new Road(7, 8, 0.3));
-
-        roads.add(new Road(2, 5, 1.0));
-        roads.add(new Road(3, 6, 1.2));
-        roads.add(new Road(1, 4, 1.1));
-
-        //serviceRequests
-        serviceRequests.add(new ServiceRequest(1, 1, "SHUTTLE", "Student pickup request at Balme Library"));
-
-        serviceRequests.add(new ServiceRequest(2, 3, "MAINTENANCE", "Broken street light near Commonwealth Hall"));
-
-        serviceRequests.add(new ServiceRequest(3, 5, "SHUTTLE", "Shuttle required at GCB"));
-
-        serviceRequests.add(new ServiceRequest(4, 7, "MAINTENANCE", "Damaged road section near N Block"));
-
-        serviceRequests.add(new ServiceRequest(5, 8, "SHUTTLE", "Student pickup request at New N Block"));
-
-        resources.add(new Resource(1, "Shuttle Bus", "Balme Library", 20, "available"));
-        resources.add(new Resource(2, "Technician-Electrical", "Maintenance Depot", 1, "available"));
+        locationDAO = new LocationDAO();
+        roadDAO = new RoadDAO();
+        serviceRequestDAO = new ServiceRequestDAO();
+        resourceDAO = new ResourceDAO();
     }
 
     @Override
     public List<Location> getLocations() {
-        return locations;
+        return locationDAO.findAll();
     }
 
     @Override
     public List<Road> getRoads() {
-        return roads;
+        return roadDAO.findAll();
     }
 
     @Override
     public List<ServiceRequest> getServiceRequests() {
-        return serviceRequests;
+        return serviceRequestDAO.findAll();
     }
 
     @Override
     public List<Resource> getResources() {
-        return resources;
+        return resourceDAO.findAll();
     }
 }
